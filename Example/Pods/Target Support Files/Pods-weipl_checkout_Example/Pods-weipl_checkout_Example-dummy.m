03bf274efa3ed4c4cfbe51d97e10733055f0f067
worldline-ashish <ashish.palaskar@worldline.com>
2023-02-10 14:29:46 +0530

    removed example

--- Example/Pods/Target Support Files/Pods-weipl_checkout_Example/Pods-weipl_checkout_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_weipl_checkout_Example : NSObject
-@end
-@implementation PodsDummy_Pods_weipl_checkout_Example
-@end
--- Example/Pods/Target Support Files/weipl_checkout/weipl_checkout-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_weipl_checkout : NSObject
-@end
-@implementation PodsDummy_weipl_checkout
-@end
