ad37f6837ca8fd9be2e1d0d4c40d6be2b04b170d
worldline-ashish <ashish.palaskar@worldline.com>
2023-02-13 12:02:58 +0530

    commit new changes

--- Package.swift
+++ Package.swift
@@ -13,8 +13,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-         .package(url: "https://github.com/Worldline-ePayments-India/weipl-checkout-ios.git", from: "1.1.1"),
-         exclude: ("Info.plist"),
+         .package(url: "https://github.com/Worldline-ePayments-India/weipl-checkout-ios.git")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
