42bcf395ac33b6996d690d6a3dea35c202878698
worldline-ashish <ashish.palaskar@worldline.com>
2023-02-14 13:10:01 +0530

    Changes done in SPM

--- Package.swift
+++ Package.swift
@@ -14,6 +14,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
          .package(url: "https://github.com/Worldline-ePayments-India/weipl-checkout-ios.git", from: "1.1.5"),
+         .package(path: "weipl_checkout/weipl_checkout.framework")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -21,9 +22,6 @@ let package = Package(
         .target(
             name: "weipl_checkout",
             dependencies: []),
-            resources: [
-                .copy("weipl_checkout/weipl_checkout.framework")
-            ]
         .testTarget(
             name: "weipl_checkoutTests",
             dependencies: ["weipl_checkout"]),
