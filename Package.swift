1fb6e8a656c22f1f4a3b60cb4e4ae8189ff11cea
worldline-ashish <ashish.palaskar@worldline.com>
2023-02-14 22:41:16 +0530

    commit changes

--- Package.swift
+++ Package.swift
@@ -14,9 +14,11 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .executableTarget(
+        .target(
             name: "weipl-checkout-ios",
-            dependencies: [], 
+            dependencies: [],
+            linkerSettings: [
+                .linkedFramework("weipl_checkout")], 
             path: "Sources"),),
         .testTarget(
             name: "weipl-checkout-iosTests",
