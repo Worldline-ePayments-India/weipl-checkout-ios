aa8bd2f12b5dcda626aca86e4f0bb805e02af756
worldline-ashish <ashish.palaskar@worldline.com>
2023-02-15 02:33:17 +0530

    commit changes

--- Package.swift
+++ Package.swift
@@ -5,13 +5,13 @@ import PackageDescription
 
 let package = Package(
     name: "weipl_checkout",
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "weipl_checkout",
-            type: .dynamic,
-            targets: ["weipl_checkout"]),
-    ],
+    // products: [
+    //     // Products define the executables and libraries a package produces, and make them visible to other packages.
+    //     .library(
+    //         name: "weipl_checkout",
+    //         type: .dynamic,
+    //         targets: ["weipl_checkout"]),
+    // ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: "https://github.com/Worldline-ePayments-India/weipl-checkout-ios.git", from: "1.1.5"),
