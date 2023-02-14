d74d3d3a9634496aa35326eefdaaf36255c65d01
worldline-ashish <ashish.palaskar@worldline.com>
2023-02-15 01:24:56 +0530

    commit new changes

--- Package.swift
+++ Package.swift
@@ -5,10 +5,6 @@ import PackageDescription
 
 let package = Package(
     name: "weipl_checkout",
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        
-    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: "https://github.com/Worldline-ePayments-India/weipl-checkout-ios.git", from: "1.1.5"),
