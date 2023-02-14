b362892e85bcb8d606853504b908d3f74b73f47d
worldline-ashish <ashish.palaskar@worldline.com>
2023-02-15 01:22:43 +0530

    commit changes

--- Package.swift
+++ Package.swift
@@ -7,10 +7,7 @@ let package = Package(
     name: "weipl_checkout",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "weipl_checkout",
-            type: .dynamic,
-            targets: ["weipl_checkout"]),
+        
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
