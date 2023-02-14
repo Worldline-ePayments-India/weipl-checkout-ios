84b8098b4cb0d43e017829664da4f8118354512c
worldline-ashish <ashish.palaskar@worldline.com>
2023-02-15 01:45:04 +0530

    commit changes

--- weipl_checkout/weipl_checkout/Package.swift
+++ weipl_checkout/weipl_checkout/Package.swift
@@ -5,10 +5,16 @@ import PackageDescription
 
 let package = Package(
     name: "weipl_checkout",
-    platforms: [ .iOS(.v12) ],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "weipl_checkout",
+            type: .dynamic,
+            targets: ["weipl_checkout"]),
+    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: "https://github.com/Worldline-ePayments-India/weipl-checkout-ios.git", from: "1.1.5"),
+         .package(url: "https://github.com/Worldline-ePayments-India/weipl-checkout-ios.git", from: "1.1.5"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -18,7 +24,6 @@ let package = Package(
             dependencies: []),
         .testTarget(
             name: "weipl_checkoutTests",
-            dependencies: [],
-            exclude: ["Sources"]),
+            dependencies: ["weipl_checkout"]),
     ]
 )
