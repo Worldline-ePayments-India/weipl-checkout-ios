d1336fc03647898ec1cd3b336edfd1be74d544a7
worldline-ashish <ashish.palaskar@worldline.com>
2023-02-14 13:36:59 +0530

    commit new changes in SPM

--- weipl_checkout/Package.swift
+++ weipl_checkout/Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "weipl_checkout",
+    defaultLocalization: "en",
+      platforms: [
+        .iOS(.v12)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -18,11 +22,11 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        
         .target(
             name: "weipl_checkout",
-            dependencies: []),
-        .testTarget(
-            name: "weipl_checkoutTests",
-            dependencies: ["weipl_checkout"]),
-    ]
+            dependencies: [],
+            path: "weipl_checkout/weipl_checkout.framework",
+            exclude: ["Info.plist"],
+        )
 )
