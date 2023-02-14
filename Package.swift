8977106aea3924a0025ff442ebbd974a181f8565
worldline-ashish <ashish.palaskar@worldline.com>
2023-02-14 16:16:03 +0530

    commit changes in SPM

--- Package.swift
+++ Package.swift
@@ -14,17 +14,23 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/Worldline-ePayments-India/weipl-checkout-ios.git", from: "1.1.5"),
+       // .package(url: "https://github.com/Worldline-ePayments-India/weipl-checkout-ios.git", from: "1.1.5"),
     ],
     
     targets: [
         .target(
             name: "weipl_checkout",
+            exclude: ["README.md"],
             dependencies: [],
             resources: [
                 .copy("info.plist")
             ]           
         )
+        .binaryTarget(
+            name: "SomeRemoteBinaryPackage",
+            url: "https://github.com/Worldline-ePayments-India/weipl-checkout-ios/releases/download/1.1.5/weipl_checkout.framework.zip",
+           // checksum: "The checksum of the XCFramework inside the ZIP archive."
+        ),
         .binaryTarget(
             name: "weipl_checkout.framework",
             path: "weipl_checkout/weipl_checkout.framework"
