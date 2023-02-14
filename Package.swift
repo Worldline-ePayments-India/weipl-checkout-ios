412089dd9b2aba934c0209774fddceb4d55e8b92
worldline-ashish <ashish.palaskar@worldline.com>
2023-02-14 16:29:43 +0530

    commit SPM changes

--- Package.swift
+++ Package.swift
@@ -1,24 +1,22 @@
-// swift-tools-version:5.5
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
     name: "weipl-checkout-ios",
-    products: [
-        .library(
-            name: "weipl_checkout",
-            targets: ["weipl_checkout"]
-        )
-    ],
     dependencies: [
-        .package(url: "https://github.com/Worldline-ePayments-India/weipl-checkout-ios.git", from: "1.1.5"),
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
     ],
-    
     targets: [
-        .target(
-            name: "weipl_checkout",
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .executableTarget(
+            name: "weipl-checkout-ios",
             dependencies: []),
         .testTarget(
             name: "weipl-checkout-iosTests",
-            dependencies: ["weipl_checkout"]),
+            dependencies: ["weipl-checkout-ios"]),
     ]
 )
