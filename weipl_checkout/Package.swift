419e4bdccf046e992344d34fb212a844a615611a
worldline-ashish <ashish.palaskar@worldline.com>
2023-02-14 13:31:16 +0530

    commit new changes in SPM

--- weipl_checkout/Package.swift
+++ weipl_checkout/Package.swift
@@ -4,26 +4,25 @@
 import PackageDescription
 
 let package = Package(
-    name: "weipl-checkout-ios",
+    name: "weipl_checkout",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "weipl-checkout-ios",
-            targets: ["weipl-checkout-ios"]),
+            name: "weipl_checkout",
+            targets: ["weipl_checkout"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-         .package(url: "https://github.com/Worldline-ePayments-India/weipl-checkout-ios.git", from: "1.1.5"),
-         .package(path: "Sources/weipl_checkout")
+         .package(url: "https://github.com/Worldline-ePayments-India/weipl-checkout-ios.git", from: "1.1.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "weipl-checkout-ios",
+            name: "weipl_checkout",
             dependencies: []),
         .testTarget(
-            name: "weipl-checkout-iosTests",
-            dependencies: ["weipl-checkout-ios"]),
+            name: "weipl_checkoutTests",
+            dependencies: ["weipl_checkout"]),
     ]
 )
