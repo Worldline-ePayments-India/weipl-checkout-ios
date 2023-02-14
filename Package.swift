d81cc633e90c9dddd57e7c7bbf17912398f90c5a
worldline-ashish <ashish.palaskar@worldline.com>
2023-02-14 16:17:08 +0530

    commit changes

--- Package.swift
+++ Package.swift
@@ -27,11 +27,6 @@ let package = Package(
             ]           
         )
         .binaryTarget(
-            name: "SomeRemoteBinaryPackage",
-            url: "https://github.com/Worldline-ePayments-India/weipl-checkout-ios/releases/download/1.1.5/weipl_checkout.framework.zip",
-           // checksum: "The checksum of the XCFramework inside the ZIP archive."
-        ),
-        .binaryTarget(
             name: "weipl_checkout.framework",
             path: "weipl_checkout/weipl_checkout.framework"
         )
