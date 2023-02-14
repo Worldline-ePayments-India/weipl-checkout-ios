d7d7792823bb170c72931617a31568ae6288540f
worldline-ashish <ashish.palaskar@worldline.com>
2023-02-15 02:28:30 +0530

    commit SMP new changes

--- Sources/weipl_checkout/weipl_checkout.swift
+++ Sources/weipl_checkout/weipl_checkout.swift
@@ -1,8 +1,6 @@
-@main
 public struct weipl_checkout {
     public private(set) var text = "Hello, World!"
 
-    public static func main() {
-        //print(weipl_checkout().text)
+    public init() {
     }
 }
