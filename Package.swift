a0b17ecd5c7c662ea76c26c4a283eeb6db6b1569
worldline-ashish <ashish.palaskar@worldline.com>
2023-02-14 23:56:43 +0530

    commit changes

--- Package.swift
+++ Package.swift
@@ -21,7 +21,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "weipl-checkout-ios",
-            dependencies: []),
+            dependencies: []
+            path: "Sources"),
         .testTarget(
             name: "weipl-checkout-iosTests",
             dependencies: ["weipl-checkout-ios"]),
