5df2bcd671543c4af88df194c92d837f2d72c66f
worldline-ashish <ashish.palaskar@worldline.com>
2023-02-15 01:00:11 +0530

    commit changes

--- Package.swift
+++ Package.swift
@@ -7,16 +7,14 @@ let package = Package(
     name: "weipl-checkout-ios",
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+         .package(url: "", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .executableTarget(
             name: "weipl-checkout-ios",
-            dependencies: []),
-        .testTarget(
-            name: "weipl-checkout-iosTests",
-            dependencies: ["weipl-checkout-ios"]),
+            dependencies: [],
+            path: "Sources"),
     ]
 )
