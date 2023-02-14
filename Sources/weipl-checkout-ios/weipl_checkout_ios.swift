403eaaf2276404e45eb8aef3aa17103302ecaf0e
worldline-ashish <ashish.palaskar@worldline.com>
2023-02-15 00:51:49 +0530

    commit changes

---
+++ Sources/weipl-checkout-ios/weipl_checkout_ios.swift
@@ -0,0 +1,8 @@
+@main
+public struct weipl_checkout_ios {
+    public private(set) var text = "Hello, World!"
+
+    public static func main() {
+        print(weipl_checkout_ios().text)
+    }
+}
---
+++ Tests/weipl-checkout-iosTests/weipl_checkout_iosTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import weipl_checkout_ios
+
+final class weipl_checkout_iosTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(weipl_checkout_ios().text, "Hello, World!")
+    }
+}
