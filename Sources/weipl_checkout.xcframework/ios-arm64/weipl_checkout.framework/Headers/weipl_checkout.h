1c0fee028f71ee8ce606e80580901695ac5d0ecf
Ashish Palaskar <ashish.palaskar@worldline.com>
2025-05-07 13:13:03 +0530

    FIX: Preload optimisation

---
+++ Sources/weipl_checkout.xcframework/ios-arm64/weipl_checkout.framework/Headers/weipl_checkout.h
@@ -0,0 +1,17 @@
+//
+//  weipl_checkout.h
+//  weipl_checkout
+//
+//  Created by Wordline ePayments India on 19/08/19.
+//  Copyright © 2019 Tech Process. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for weipl_checkout.
+FOUNDATION_EXPORT double weipl_checkoutVersionNumber;
+
+//! Project version string for weipl_checkout.
+FOUNDATION_EXPORT const unsigned char weipl_checkoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <weipl_checkout/PublicHeader.h>
